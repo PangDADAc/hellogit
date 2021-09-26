848f510136545c3b89fb4c901e4c7a8fc5603d18
Administrator <1263327431@qq.com>
2021-09-26 16:44:28 +0800

    添加文件

---
+++ src/main/java/hello.java
@@ -0,0 +1,10 @@
+/**
+ * @version 1.0
+ * @author: NieXuDong
+ * @date: 2021年09月26日 16:37
+ */
+public class hello {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+}
